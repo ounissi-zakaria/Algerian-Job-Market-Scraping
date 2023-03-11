be36d3ffb2a8b3fcc371dd664583c68d37456555
Zakaria Ounissi <ounissizakaria@gmail.com>
2023-03-11 12:19:44 +0100

    Add description spider

--- Algerian_Job_Market/spiders/trustme_work_description.py
+++ Algerian_Job_Market/spiders/trustme_work_description.py
@@ -1,23 +1,27 @@
 from scrapy import Request, Spider
+import pandas as pd
+from Algerian_Job_Market.items import JobItem
+
 
 class TrustmeWorkDescriptionSpider(Spider):
-    name = 'trustme_work_description'
+    name = 'trustme.work_description'
     allowed_domains = ['trustme.work']
 
-    def __init__(self):
-        self.BASE_URL = ("https://api.trustme.work/api/job_offers/%(job_hash)s"
+    def __init__(self, load_jobs_from):
+        self.BASE_URL = ("https://api.trustme.work/api/job_offers/%(id)s"
                          "?include=technologies,job,contract_type,company")
 
-    def start_requests(sel):
-        jobs_list = []
-        for job in jobs_list:
-            yield Request(
-                    url=self.BASE_URL % job,
-                    callback=self.parse_job_description,
-                    cb_kwargs={"job": job}
-                    )
+        self.jobs_list = self.load_jobs(load_from=load_jobs_from)
 
+    def start_requests(self):
+        for job in self.jobs_list:
+            yield Request(url=self.BASE_URL % job, callback=self.parse_job_description, cb_kwargs={"job": job})
 
-    def parse_job_description(self, response):
-        ...
+    def parse_job_description(self, response, job):
+        item = JobItem(**job)
+        item.description = response.json()["data"]["description"]
+        yield item
 
+    def load_jobs(self, load_from: str):
+        for idx, row in pd.read_csv(load_from).iterrows():
+            yield row
