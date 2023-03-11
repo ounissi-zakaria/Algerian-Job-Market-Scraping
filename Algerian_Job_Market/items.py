dfb16082d6f60867d397d4beb83dda3f0a73afed
Zakaria Ounissi <ounissizakaria@gmail.com>
2023-03-11 12:18:08 +0100

    Add description field

--- Algerian_Job_Market/items.py
+++ Algerian_Job_Market/items.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Optional
 
+
 @dataclass
 class JobItem:
     title: str = field(default="")
@@ -9,7 +10,7 @@ class JobItem:
     link: str = field(default="")
 
     published_at: Optional[datetime] = field(default=None)
-    created_at: Optional[datetime]= field(default=None)
+    created_at: Optional[datetime] = field(default=None)
 
     technologies: list[str] = field(default_factory=list)
 
@@ -17,3 +18,5 @@ class JobItem:
     contract_type: str = field(default="")
     job_type: str = field(default="")
     level: str = field(default="")
+
+    description: str = field(default="")
