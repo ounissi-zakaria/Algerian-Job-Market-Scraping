ad11920c70184ffe512464abd3523c1a8a215500
Zakaria Ounissi <ounissizakaria@gmail.com>
2023-03-13 00:03:21 +0100

    Setup zyted

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Automatically created by: scrapyd-deploy
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Algerian-Job-Market',
+    version='1.0',
+    packages=find_packages(),
+    entry_points={'scrapy': ['settings = Algerian_Job_Market.settings']},
+)
